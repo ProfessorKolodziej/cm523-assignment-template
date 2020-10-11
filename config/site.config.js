13745b64d8452a1ee2a74314830ee58837d232f5
Ashley Kolodziej <kolodz10@bu.edu>
2020-10-11 18:17:37 -0400

    Add deploy to Github Pages ability

--- config/site.config.js
+++ config/site.config.js
@@ -15,7 +15,7 @@ const config = {
   site_description: 'A modern boilerplate for static website development',
 
   // Your website's URL, used for sitemap
-  site_url: '',
+  site_url: 'https://professorkolodziej.github.io/static-site-boilerplate/',
 
   // Google Analytics tracking ID (leave blank to disable)
   googleAnalyticsUA: '',
